3391ca762003a8671a3a3a215c3ed81b7542331f
dalechyn <vladyslav@spilnota.xyz>
2024-10-12 20:57:21 +0300

    nit: playground update

--- playground/index.ts
+++ playground/index.ts
@@ -2,7 +2,10 @@ import { http, createPublicClient } from 'viem'
 import { publicViemQuoterActions } from 'viem-quoter'
 import { mainnet } from 'viem/chains'
 
-const _publicClient = createPublicClient({
+const publicClient = createPublicClient({
   chain: mainnet,
   transport: http(),
 }).extend(publicViemQuoterActions)
+
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+console.log('ETH Price is: ', await publicClient.getUniswapV3EthPrice())
